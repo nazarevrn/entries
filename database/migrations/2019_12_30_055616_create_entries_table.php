1de399fac5571f7f161605109f737500bee471ef
Назарьев Роман <r.nazarev@efko.ru>
2019-12-30 09:02:03 +0300

    init

---
+++ database/migrations/2019_12_30_055616_create_entries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('staff_id', false, true); //unsigned
+            $table->foreign('staff_id')->references('id')->on('staff');
+            $table->timestamp('timestamp');
+            $table->tinyInteger('pass_direction')->comment('0 - in, 1 - out');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
